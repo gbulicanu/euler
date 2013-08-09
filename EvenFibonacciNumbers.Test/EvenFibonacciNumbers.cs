00c7017f0292a22f5963a64b244b5e8e60e530ca
Gheorghe Bulicanu <gicu@live.com>
2013-08-09 22:18:43 +0300

    Changed test project naming to execute on build server

--- EvenFibonacciNumbers.Test/EvenFibonacciNumbers.cs
+++ EvenFibonacciNumbers.Test/EvenFibonacciNumbers.cs
@@ -7,7 +7,7 @@
  * By considering the terms in the Fibonacci sequence whose values do not exceed four million,
  * find the sum of the even-valued terms.
  */
-namespace EvenFibonacciNumbers
+namespace EvenFibonacciNumbers.Test
 {
   using System;
   using Microsoft.VisualStudio.TestTools.UnitTesting;
