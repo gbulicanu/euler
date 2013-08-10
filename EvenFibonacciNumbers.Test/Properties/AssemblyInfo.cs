ecfb319a61bfc380ca69a314a1a52582b980ce4b
Gheorghe Bulicanu <gicu@live.com>
2013-08-10 04:48:48 +0300

    Changed assembly info metadata

--- EvenFibonacciNumbers.Test/Properties/AssemblyInfo.cs
+++ EvenFibonacciNumbers.Test/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EvenFibonacciNumbers")]
+[assembly: AssemblyTitle("EvenFibonacciNumbers.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EvenFibonacciNumbers")]
+[assembly: AssemblyProduct("Euler")]
 [assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
